6c348277db7398ba9b7ad35df4b288216b2dfb84
WackyMole <wacky@wackymole.com>
2022-09-21 16:29:26 -0500

    CustomAltar Mod fix for Hun

--- MagicPortalFluid/RareMagicPortal.cs
+++ MagicPortalFluid/RareMagicPortal.cs
@@ -32,8 +32,7 @@ using YamlDotNet;
 using YamlDotNet.Serialization;
 using LocalizationManager;
 using StatusEffectManager;
-
-
+using LitJson;
 
 namespace RareMagicPortal
 {
@@ -1191,8 +1190,249 @@ namespace RareMagicPortal
 			}
 		}
 
+		public static string configPath = Path.Combine(BepInEx.Paths.ConfigPath, $"{ModGUID}.json");
+		[HarmonyPatch(typeof(OfferingBowl), "Awake")]
+		public static class AlterOfferBowlAwake
+		{
+			public static void Postfix(OfferingBowl __instance)
+			{
+				if (__instance == null) return;
+				var altarConfigs = GetJson();
 
-		[HarmonyPatch(typeof(Player), "CheckCanRemovePiece")]
+				foreach (var config in altarConfigs)
+				{
+					try
+					{
+						if (GameObject.Find(config.AltarPrefabName + "(Clone)") != null)
+						{
+							var prefab = GameObject.Find(config.AltarPrefabName + "(Clone)");
+							
+							prefab.GetComponentInChildren<OfferingBowl>().m_name = config.Name;
+							prefab.GetComponentInChildren<OfferingBowl>().m_bossPrefab = ZNetScene.instance.GetPrefab(config.BossPrefab).gameObject;
+							prefab.GetComponentInChildren<OfferingBowl>().m_bossItem = ZNetScene.instance.GetPrefab(config.SacrificeItem).GetComponent<ItemDrop>();
+							prefab.GetComponentInChildren<OfferingBowl>().m_bossItems = config.SacrificeAmount;
+							var ItemsEmpty = new List<ItemDrop>();
+							if (prefab.GetComponentsInChildren<ItemStand>(true).Count() == null)  //never because this gets overwritten
+							{
+								Debug.Log("Itemstand exist");
+								Debug.Log(config.SacrificeItem);
+								Debug.Log(prefab.GetComponentsInChildren<ItemStand>(true).Count());
+
+
+								Debug.Log(ZNetScene.instance.GetPrefab(config.SacrificeItem).GetComponent<ItemDrop.ItemData>());
+								var Items = new List<ItemDrop>
+								{
+									new ItemDrop
+										{
+											m_itemData = ZNetScene.instance.GetPrefab(config.SacrificeItem).GetComponent<ItemDrop.ItemData>()
+										 }
+								};
+
+								var ItemStands = prefab.GetComponentsInChildren<ItemStand>(true);
+								foreach (var ItemStand in ItemStands)
+                                {
+									Debug.Log($"Itemstand  set to {config.SacrificeItem}");
+									ItemStand.m_supportedItems = Items;
+								}
+								
+							}
+						}
+					}
+					catch (Exception e)
+					{
+						Debug.LogError($"Loading config for {config.AltarPrefabName + "(Clone)"} failed. {e.Message}");
+					}
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(ItemStand), "CanAttach")]
+		public static class ItemStandUseItemAttach
+		{
+			public static void Postfix(ItemStand __instance, ItemDrop.ItemData item, ref bool __result)
+			{
+				//Debug.Log($"Postfix CanAttach");
+				if (__instance.GetAttachPrefab(item.m_dropPrefab) == null)
+				{
+					Debug.Log($"Overwritting Attach Prefab in CanAttach, hopefully it has an attach- otherwise...");
+					__result = true; 
+				}
+
+				if (__instance.IsUnsupported(item))
+				{
+					Debug.Log($"CanAttach item is unsupported");
+					__result = false;
+					return;
+				}
+
+				if (!__instance.IsSupported(item))
+				{
+					Debug.Log($"CanAttach IsSupported is false");
+					__result = false;
+					return;
+				}
+
+				if (__instance.m_supportedTypes.Count == 0)
+				{
+					Debug.Log($"CanAttach types = 0 ");
+					//return true;
+				}
+				if (__instance.m_supportedTypes.Contains(item.m_shared.m_itemType))
+                {
+					//Debug.Log($"This is not a support Type");
+					//__result = false;
+					//return;
+
+				}
+
+				Debug.Log($"CanAttach Approved");
+				__result = true;//  items have itemTypes that it also checks for which is stupid
+			}
+		}
+
+		[HarmonyPatch(typeof(ItemStand), "Awake")]
+		public static class ItemStandAwake
+		{
+			public static void Postfix(ItemStand __instance)
+			{
+				var altarConfigs = GetJson();
+				var Items = new List<ItemDrop>();
+				//var ItemsType = new List<ItemDrop.ItemData.ItemType>();
+
+				foreach (var config in altarConfigs)
+				{
+					try
+					{
+						var stand = config.ItemStandName + "(Clone)";
+						//Debug.Log($"Itemstand looking {__instance.gameObject.name} with config {config.ItemStandName}");
+
+						if (__instance.gameObject.name == stand && config.ItemStandName != null)
+						{
+							var scraficeitem = ObjectDB.instance.GetItemPrefab(config.SacrificeItem);
+							var ItemDrop = scraficeitem.GetComponentInChildren<ItemDrop>(true);
+							var ItemData = ItemDrop.m_itemData;
+							//var ItemType = ItemData.m_shared.m_itemType;
+							if (Items.Count() == 0)
+								Items.Add(ItemDrop);
+							//if (ItemsType.Count() == 0 )
+							//	ItemsType.Add(ItemType);
+
+							__instance.m_supportedItems = Items;
+							Debug.Log($"Itemstand exist for {__instance.m_name} with object name {__instance.gameObject.name} setting to {config.SacrificeItem} with name {ItemData.m_shared.m_name}");
+						}
+					}
+					catch (Exception e) { Debug.LogError($"Loading config for {config.ItemStandName + "(Clone)"} failed. {e.Message}"); }
+
+				}
+			}
+		}
+
+		private void LoadConfig()
+		{
+			if (!File.Exists(configPath))
+			{
+				GenerateConfigFile();
+				Debug.Log("Generated new configs");
+				return;
+			}
+		}
+		private void GenerateConfigFile()
+		{
+			var altarConfigs = new List<AltarConfig>();
+
+			// Vanilla
+			var MeadowsAltarConfig = new AltarConfig();
+			MeadowsAltarConfig.AltarPrefabName = "Eikthyrnir";
+			MeadowsAltarConfig.Name = "Mystical altar";
+			MeadowsAltarConfig.BossPrefab = "Eikthyr";
+			MeadowsAltarConfig.SacrificeItem = "TrophyDeer";
+			MeadowsAltarConfig.SacrificeAmount = 2;
+			altarConfigs.Add(MeadowsAltarConfig);
+
+			var BlackForestAltarConfig = new AltarConfig();
+			BlackForestAltarConfig.AltarPrefabName = "GDKing";
+			BlackForestAltarConfig.Name = "Ancient bowl";
+			BlackForestAltarConfig.BossPrefab = "gd_king";
+			BlackForestAltarConfig.SacrificeItem = "AncientSeed";
+			BlackForestAltarConfig.SacrificeAmount = 3;
+			altarConfigs.Add(BlackForestAltarConfig);
+
+			var SwampAltarConfig = new AltarConfig();
+			SwampAltarConfig.AltarPrefabName = "Bonemass";
+			SwampAltarConfig.Name = "Boiling death";
+			SwampAltarConfig.BossPrefab = "Bonemass";
+			SwampAltarConfig.SacrificeItem = "WitheredBone";
+			SwampAltarConfig.SacrificeAmount = 10;
+			altarConfigs.Add(SwampAltarConfig);
+
+			var MountainsAltarConfig = new AltarConfig();
+			MountainsAltarConfig.AltarPrefabName = "Dragonqueen";
+			MountainsAltarConfig.Name = "Sacrificial altar";
+			MountainsAltarConfig.BossPrefab = "Dragon";
+			MountainsAltarConfig.SacrificeItem = "DragonEgg";
+			MountainsAltarConfig.SacrificeAmount = 1;
+			MountainsAltarConfig.ItemStandName = "dragoneggcup";
+			altarConfigs.Add(MountainsAltarConfig);
+
+			var PlainsAltarConfig = new AltarConfig();
+			PlainsAltarConfig.AltarPrefabName = "GoblinKing";
+			PlainsAltarConfig.Name = "Mystical altar";
+			PlainsAltarConfig.BossPrefab = "GoblinKing";
+			PlainsAltarConfig.SacrificeItem = "GoblinTotem";
+			PlainsAltarConfig.SacrificeAmount = 1;
+			PlainsAltarConfig.ItemStandName = "goblinking_totemholder";
+			altarConfigs.Add(PlainsAltarConfig);
+
+
+			// EVA
+			var MistlandsAltarConfig = new AltarConfig();
+			MistlandsAltarConfig.AltarPrefabName = "SvartalfrQueenAltar_New";
+			MistlandsAltarConfig.Name = "Cursed Altar";
+			MistlandsAltarConfig.BossPrefab = "SvartalfarQueen";
+			MistlandsAltarConfig.SacrificeItem = "CursedEffigy";
+			MistlandsAltarConfig.SacrificeAmount = 5;
+			altarConfigs.Add(MistlandsAltarConfig);
+
+			var DeepNorthAltarConfig = new AltarConfig();
+			DeepNorthAltarConfig.AltarPrefabName = "JotunnAltar";
+			DeepNorthAltarConfig.Name = "Frozen Altar";
+			DeepNorthAltarConfig.BossPrefab = "Jotunn";
+			DeepNorthAltarConfig.SacrificeItem = "YmirsSoulEssence";
+			DeepNorthAltarConfig.SacrificeAmount = 5;
+			altarConfigs.Add(DeepNorthAltarConfig);
+
+			var AshlandsAltarConfig = new AltarConfig();
+			AshlandsAltarConfig.AltarPrefabName = "BlazingDamnedOneAltar";
+			AshlandsAltarConfig.Name = "Blazing Altar";
+			AshlandsAltarConfig.BossPrefab = "HelDemon";
+			AshlandsAltarConfig.SacrificeItem = "FenrirsHeart";
+			AshlandsAltarConfig.SacrificeAmount = 5;
+			altarConfigs.Add(AshlandsAltarConfig);
+
+
+			var jsonText = JsonMapper.ToJson(altarConfigs);
+			File.WriteAllText(configPath, jsonText);
+		}
+		internal static List<AltarConfig> GetJson()
+		{
+			var jsonText = File.ReadAllText(configPath);
+			var altarConfigs = JsonMapper.ToObject<List<AltarConfig>>(jsonText);
+			return altarConfigs;
+		}
+	
+	[Serializable]
+	public class AltarConfig
+	{
+		public string AltarPrefabName { get; set; }
+		public string Name { get; set; }
+		public string BossPrefab { get; set; }
+		public string SacrificeItem { get; set; }
+		public int SacrificeAmount { get; set; }
+		public string ItemStandName { get; set; }
+		}
+
+
+	[HarmonyPatch(typeof(Player), "CheckCanRemovePiece")]
 		private static class Player_CheckforOwnerP
 		{
 			[HarmonyPrefix]
@@ -1305,7 +1545,7 @@ namespace RareMagicPortal
 			english.SetupLanguage("English");
 			LoadAssets();
 
-			
+			LoadConfig();
 
 			assetPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), typeof(MagicPortalFluid).Namespace);
 			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), (string)null);
